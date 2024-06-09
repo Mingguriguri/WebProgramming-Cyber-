065f20fa5e1723c028c4d7e94e5c5049f90e9911
Mingguriguri <rlaalswjd-@naver.com>
2024-06-09 13:09:56 +0900

    W09: JSX  문법 규칙 이해하기

--- W09-react/hello-react/src/App.js
+++ W09-react/hello-react/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import MyHello from './MyHello';
 
 function App() {
+  const name = '김민정';
   return (
+    // JSX 문법규칙2 - 여러 개의 태그는 하나의 태그로 묶여야 한다. (지금의 div태그처럼)
     <div className="App">
       <h1> Hello This is WebProgramming lecture</h1>
+      {/* JSX 문법규칙1 - 태그는 반드시 닫아야 한다. self-closing 태그가 가능하다. */}
+      {/* <br> */}
+      <br /> 
+      {/* JSX 문법규칙3 - 변수명은 {}로 감싼다 */}
+      {/* JSX 문법규칙4 - CSS의 클래스(class) 스타일은 className으로 설정한다. */}
+      <h3 className="box"> Hello, {name} </h3>
+
       <MyHello />
       <MyHello />
-      <MyHello />
-      <MyHello />
-      <MyHello />
+      {/* 주석 */}
     </div>
   );
 }
