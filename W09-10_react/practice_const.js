78321eb9e88e36d7a102783113acd16131d6e91f
Mingguriguri <rlaalswjd-@naver.com>
2024-06-10 14:03:32 +0900

    W10: 자바스크립트 문법 - 상수(const)

---
+++ W09-10_react/practice_const.js
@@ -0,0 +1,13 @@
+/*
+자바스크립트 상수 선언:
+const키워드 / 상수를 선언하고 나면 값을 바꿀 수 없음
+*/
+const value = 35;
+console.log(value)
+
+
+// value = value + 50;
+// console.log(value)
+
+// value = 100;
+// console.log(value)
