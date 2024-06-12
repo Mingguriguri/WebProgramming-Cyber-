78f21524a1c657988ff7a06cb6319aabe9995e87
Mingguriguri <rlaalswjd-@naver.com>
2024-06-12 19:51:47 +0900

    W11: ## useState: 여러 개 입력 상자(input)의 값 관리- useState()

--- W11-react/input-react/src/InputSample.js
+++ W11-react/input-react/src/InputSample.js
@@ -9,16 +9,44 @@ function InputSample(){
         border: '1px solid darkgrey'
     };
 
+    // useState(): 값이 변화되는 것을 저장
+    const [inputs, setInputs] = useState({
+        username:'',
+        useremail:''
+    });
+
+    // 비구조화 할당을 통해 객체 값 추출
+    const {username, useremail} = inputs;
+
+    // 상태값이 변화되는 함수: 타겟대상 2개 처리
+    const onDataChange = (e) => {
+        const {value, name} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    }
+    // 상태값을 초기화시키는 함수
+    const onDataReset = () => {
+        setInputs({
+            username:'',
+            useremail:''
+        });
+    }
     return (
         <div style={box}>
             <input  type="text" 
                     placeholder="이륾 입력"
-                    name="username" />
+                    name="username" 
+                    value={username}
+                    onChange={onDataChange}/>
             <input  type="text"
                     placeholder="이메일 입력"
-                    name="useremail" />
-            <button >초기화</button>
-            <h3>값: 이름(이메일)</h3>
+                    name="useremail" 
+                    value={useremail}
+                    onChange={onDataChange}/>
+            <button onClick={onDataReset}>초기화</button>
+            <h3>값: {username}({useremail})</h3>
         </div>
     );
 }
